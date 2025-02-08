0efbbf2010ad7532557299e508e7dcdfa428601d
py0o0 <pantom0425@naver.com>
2025-02-08 21:07:47 +0900

    auth connect

--- src/main/java/com/example/gateway/GatewayApplication.java
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -3,6 +3,9 @@ package com.example.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,17 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator ecomRouteLocator(RouteLocatorBuilder builder) {
+        System.out.println("게이트웨이에서 개별 서비스 URL 등록");
+        return builder.routes()
+                // 개별 라우트 등록
+                // 서비스별 URL 별칭이 1개인 경우, n개인 경우도 존재
+                .route("auth",
+                        r -> r.path("/auth/**").uri("lb://auth"))
+                .build();
     }
 
 }
--- src/main/java/com/example/gateway/config/SecurityConfig.java
+++ src/main/java/com/example/gateway/config/SecurityConfig.java
@@ -1,16 +1,24 @@
 package com.example.gateway.config;
 
+import com.example.gateway.jwt.JwtFilter;
+import com.example.gateway.jwt.JwtUtil;
+import com.example.gateway.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
 @EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
@@ -21,10 +29,11 @@ public class SecurityConfig {
         http.securityContextRepository(NoOpServerSecurityContextRepository.getInstance());
 
         http.authorizeExchange(auth -> auth
-                .pathMatchers("/").permitAll()
+                .pathMatchers("/auth/login","/auth/join", "auth/reissue","/").permitAll()
                 .pathMatchers("/user/admin").hasRole("ROLE_ADMIN")
                 .anyExchange().authenticated());
 
+        http.addFilterAt(jwtFilter, SecurityWebFiltersOrder.AUTHENTICATION);
 
         return http.build();
     }
