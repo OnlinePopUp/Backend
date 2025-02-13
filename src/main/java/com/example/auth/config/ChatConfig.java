f84bae031eb146ae6f44f5b8fb1723e0e645dc84
py0o0 <pantom0425@naver.com>
2025-02-13 13:44:12 +0900

    user chat

--- src/main/java/com/example/auth/config/ChatConfig.java
+++ src/main/java/com/example/auth/config/ChatConfig.java
@@ -13,16 +13,16 @@ public class ChatConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // stomp 접속 주소 url = ws://ip/ws, 프로토콜이 http가 아님
-        registry.addEndpoint("/ws") // 연결될 엔드포인트
-                //.setAllowedOrigins("http://localhost:3000")
+        registry.addEndpoint("/chat/ws") // 연결될 엔드포인트
+                .setAllowedOrigins("http://localhost:3000")
                 .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // 메세지를 구독(수신)하는 엔드 포인트
-        registry.enableSimpleBroker("/sub");
+        registry.enableSimpleBroker("/chat/sub");
         // 메세지를 발행(송신)하는 엔드 포인트
-        registry.setApplicationDestinationPrefixes("/pub");
+        registry.setApplicationDestinationPrefixes("/chat/pub");
     }
 }
